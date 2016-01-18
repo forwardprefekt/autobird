e77ee829e8defc0443a7b57b59e9afad8380b802
gangsta <mattd@grifsec.com>
2016-01-18 04:46:17 -0500

    first attempt at birdy classifier

--- autobird.py
+++ autobird.py
@@ -9,6 +9,7 @@ import serial
 cap = cv2.VideoCapture(1)
 
 bottompipe = cv2.CascadeClassifier("bottompipe_cascade.csv")
+bird = cv2.CascadeClassifier("bird_cascade.csv")
 
 x = 1  #frame counter
 
@@ -23,6 +24,11 @@ while(True):
     	rects = bottompipe.detectMultiScale(gray, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20,20))
 	for (x,y,w,h) in rects:
 		cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+
+    	rects = bird.detectMultiScale(gray, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20,20))
+	for (x,y,w,h) in rects:
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,255),2)
+
     	#if len(rects) > 0:
 		##cv2.rectangle(frame,rects);
 		#print "booom!!!!";

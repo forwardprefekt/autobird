ef449ad547b8bdbdefe228546007abf30d06da1c
gangsta <mattd@grifsec.com>
2016-01-17 23:48:27 -0500

    this should draw rectangles around detected objects

--- autobird.py
+++ autobird.py
@@ -21,9 +21,7 @@ while(True):
     	#rects = toppipe.detectMultiScale(frame, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20,20))
     	rects = toppipe.detectMultiScale(frame, 1.3, 5)
 	for (x,y,w,h) in rects:
-		img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    		roi_gray = gray[y:y+h, x:x+w]
-    		roi_color = img[y:y+h, x:x+w]
+		cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
     	#if len(rects) > 0:
 		##cv2.rectangle(frame,rects);
 		#print "booom!!!!";
